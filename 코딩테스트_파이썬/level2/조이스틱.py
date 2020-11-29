9b98350c13fe6a396333ea7f9219d7545528a6c5
hongjo <ahj0313@ajou.ac.kr>
2020-11-29 11:12:53 +0900

    [level2] 조이스틱 제일 근접한거

--- 코딩테스트_파이썬/level2/조이스틱.py
+++ 코딩테스트_파이썬/level2/조이스틱.py
@@ -8,31 +8,28 @@ def solution(name):
 
     rightSub = [i for i in sub]
     right = 0
+    left = 0
     rightNum = 0
+    leftNum = 0
     while True:
         if sum(rightSub) == 0:
             break
         if rightSub[rightNum] != 0:
             right += rightSub[rightNum]
             rightSub[rightNum] = 0
-            rightNum += 1
+        rightNum += 1
 
     right += rightNum
-
-
-    left = 0
-    leftNum = 0
-
     while True:
         if sum(sub) == 0:
             break
         if sub[leftNum] != 0:
             left += sub[leftNum]
             sub[leftNum] = 0
-            leftNum -= 1
+        leftNum -= 1
 
     left += abs(leftNum)
 
     answer = min(right, left)
 
-    return answer
+    return answer - 1
