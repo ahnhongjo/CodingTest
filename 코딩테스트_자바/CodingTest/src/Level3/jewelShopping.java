4c9162543bea891cd18e6f7cd00363006ddb5ae6
hongjo <ahj0313@ajou.ac.kr>
2021-04-02 06:10:38 +0900

    정답이다 연금술사

--- 코딩테스트_자바/CodingTest/src/Level3/jewelShopping.java
+++ 코딩테스트_자바/CodingTest/src/Level3/jewelShopping.java
@@ -6,29 +6,38 @@ import java.util.HashMap;
 
 public class jewelShopping {
 	public int[] solution(String[] gems) {
-        int[] answer = {};
         
         ArrayList<String> gemKind = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>();
         
         for(String g:gems) {
         	if(!gemKind.contains(g)) {
         		gemKind.add(g);
+        		map.put(g, 0);
         	}
         }
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for(String g:gemKind) {
-        	map.put(g, 0);
-        }
-        
+
         int min=gems.length+1;
         int start=0;
         int end=0;
+        int num=0;
+        int kind=gemKind.size();
         int[] startEnd = {0,0};
+        boolean flag=false;
         
         while(end!=gems.length) {
-    		map.put(gems[end], map.get(gems[end])+1);
-        	if(allJewel(map)) {
+        	String tmp=gems[end];
+        	int cnt=map.get(tmp);
+        	
+    		map.put(tmp, cnt+1);
+    		if(cnt==0) {
+    			num++;
+    		}
+    		
+    		if(num==kind)
+    			flag=true;
+    		
+        	if(flag) {
         		while(map.get(gems[start])>1) {
         			map.put(gems[start], map.get(gems[start])-1);
         			start++;
@@ -47,14 +56,4 @@ public class jewelShopping {
         System.out.println(Arrays.toString(startEnd));
         return startEnd;
     }
-	
-	boolean allJewel(HashMap<String,Integer> h) {
-		
-		for(int j:h.values()) {
-			if(j==0)
-				return false;
-		}
-		
-		return true;
-	}
 }
