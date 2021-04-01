62c7c76bd2a8a76abdb5f3607411966e0fa1bc22
hongjo <ahj0313@ajou.ac.kr>
2021-04-02 05:09:40 +0900

    효율성만 조금 건들면 될

--- 코딩테스트_자바/CodingTest/src/Level3/jewelShopping.java
+++ 코딩테스트_자바/CodingTest/src/Level3/jewelShopping.java
@@ -8,8 +8,6 @@ public class jewelShopping {
 	public int[] solution(String[] gems) {
         int[] answer = {};
         
-        System.out.println(Arrays.toString(gems));
-        
         ArrayList<String> gemKind = new ArrayList<>();
         
         for(String g:gems) {
@@ -17,8 +15,6 @@ public class jewelShopping {
         		gemKind.add(g);
         	}
         }
-        
-        System.out.println(gemKind);
         HashMap<String, Integer> map = new HashMap<>();
         
         for(String g:gemKind) {
@@ -26,36 +22,29 @@ public class jewelShopping {
         }
         
         int min=gems.length+1;
-        ArrayList<String> window=new ArrayList<>();
         int start=0;
         int end=0;
         int[] startEnd = {0,0};
         
         while(end!=gems.length) {
-    		window.add(gems[end]);
     		map.put(gems[end], map.get(gems[end])+1);
-    		
-    		
         	if(allJewel(map)) {
         		while(map.get(gems[start])>1) {
         			map.put(gems[start], map.get(gems[start])-1);
         			start++;
-        			window.remove(0);
         		}
         		
         		if(min>end-start+1) {
-        			min=start-end+1;
+        			min=end-start+1;
         			startEnd[0]=start;
         			startEnd[1]=end;
         		}
         	}
-        	
-        	System.out.println(window);
-        	System.out.println(Arrays.toString(startEnd));
         	end++;
         }
         startEnd[0]+=1;
         startEnd[1]+=1;
+        System.out.println(Arrays.toString(startEnd));
         return startEnd;
     }
 	
